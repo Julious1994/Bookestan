12a5ec17a35adc4c7d44c3797ec17a3cfa5418ae
Amit Patel <a.patel@axelor.com>
2020-12-25 23:01:27 +0530

    Setup app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import routes from './routes';
+import {StateProvider} from './store/store';
+import {reducer, initialState} from './store/reducer';
+
+const Stack = createStackNavigator();
+
+function App() {
+	return (
+		<StateProvider initialState={initialState} reducer={reducer}>
+			<NavigationContainer>{routes(Stack)}</NavigationContainer>
+		</StateProvider>
+	);
+}
+
+export default App;

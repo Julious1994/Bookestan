b3e08daef0cdb6f3febdc2731da19f73b09e4807
Amit Patel <a.patel@axelor.com>
2020-12-25 23:00:14 +0530

    Add images

--- src/images/imageMapper.js
+++ src/images/imageMapper.js
@@ -2,4 +2,55 @@ export default {
 	logoIcon: {
 		source: require('./logoIcon.png'),
 	},
+	instagram: {
+		source: require('./instagram.png'),
+	},
+	fb: {
+		source: require('./fb.png'),
+	},
+	twiter: {
+		source: require('./twiter.png'),
+	},
+	messages: {
+		source: require('./messages.png'),
+	},
+	lock: {
+		source: require('./lock.png'),
+	},
+	rightAngle: {
+		source: require('./rightAngle.png'),
+	},
+	categories: {
+		source: require('./categories.png'),
+	},
+	home: {
+		source: require('./home.png'),
+	},
+	profile: {
+		source: require('./profile.png'),
+	},
+	orangeLock: {
+		source: require('./lock-orange.png'),
+	},
+	back: {
+		source: require('./back.png'),
+	},
+	menu: {
+		source: require('./menu.png'),
+	},
+	cart: {
+		source: require('./cart.png'),
+	},
+	search: {
+		source: require('./search.png'),
+	},
+	moreArrow: {
+		source: require('./more-arrow.png'),
+	},
+	play: {
+		source: require('./play.png'),
+	},
+	setting: {
+		source: require('./setting.png'),
+	},
 };

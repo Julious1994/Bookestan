a59cd9bee934da817793c246f5aaf8b1ef76472e
Amit Patel <a.patel@axelor.com>
2020-12-25 23:07:30 +0530

    Setup redux store

---
+++ src/store/reducer.js
@@ -0,0 +1,87 @@
+export const initialState = {
+	token: null,
+	user: null,
+	defaultcar: null,
+	drawer: false,
+	notification: null,
+	loading: false,
+	cart: [],
+	likes: [],
+	purchasedBooks: [],
+	listenedBooks: [],
+};
+
+export const reducer = (state, action) => {
+	switch (action.type) {
+		case 'setToken':
+			return {
+				...state,
+				token: action.token,
+			};
+		case 'SET_USER':
+			return {
+				...state,
+				user: action.userData,
+			};
+		case 'setCredential':
+			return {
+				...state,
+				...action.credential,
+				credential: {...action.credential},
+			};
+		case 'TOGGELE_DRAWER':
+			return {
+				...state,
+				drawer: !state.drawer,
+			};
+		case 'Notification':
+			return {
+				...state,
+				notification: action.notification,
+			};
+		case 'SET_LIKES':
+			return {
+				...state,
+				likes: [...action.data],
+			};
+		case 'ADD_LIKE':
+			return {
+				...state,
+				likes: [...state.likes, {...action.data}],
+			};
+		case 'SET_LOADING':
+			return {
+				...state,
+				loading: action.loading,
+			};
+		case 'ADD_CART':
+			return {
+				...state,
+				cart: [...state.cart, {...action.payload}],
+			};
+		case 'SET_CART':
+			return {
+				...state,
+				cart: [...action.payload],
+			};
+		case 'REMOVE_CART':
+			const newCart = state.cart.filter((c, i) => i !== action.index);
+			return {
+				...state,
+				cart: [...newCart],
+			};
+		case 'SET_PURCHASED':
+			return {
+				...state,
+				purchasedBooks: [...action.data],
+			};
+		case 'SET_LISTENED':
+			return {
+				...state,
+				listenedBooks: [...action.data],
+			};
+		case 'CHANGE_CART':
+		default:
+			return state;
+	}
+};
